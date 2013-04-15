8da3ab4b3972919c5267f2ef06627d22afe5367e
Atul <amkcat9@gmail.com>
2013-04-15 15:46:08 +0530

    configuration

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "atul";
+$dbpassword = "mypasswd";
+$dbdatabase = "blogtastic";
+
+/*$dbhost = "mysql1.000webhost.com";
+$dbuser = "a8127869_ledzep";
+$dbpassword = "38pnpadma";
+$dbdatabase = "a8127869_blog";*/
+
+$config_blogname = "Blogsphere";
+
+$config_author = "Atul Karmarkar";
+
+$config_basedir = "http://127.0.0.1/sites/blogtastic/";
+
+?>
